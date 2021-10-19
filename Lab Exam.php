7cf8e7eabb58f1079f1d90e0b67ec1e1e9b99905
Anjum36 <91002162+Anjum36@users.noreply.github.com>
2021-10-18 19:37:21 -0800

    Add files via upload

---
+++ Lab Exam.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <html>
+	<head>
+		<title>Registration</title>
+	</head>
+	<body>
+		<h1>Please fill up the form</h1>
+		<hr/>
+		<form action="" method="post">
+			<table >
+				<tr>
+					<td>First Name:</td>
+					<td><input type="text"  name="name" ></td>
+				</tr>
+				<tr>
+					<td>Last name:</td>
+					<td><input type="text"  name="name" ></td>
+				</tr>
+				<tr>
+					<td>Email:</td>
+					<td><input type="Email" Email="name"></td>
+		</tr>
+        
+        <tr>
+					<td>Gender:</td>
+					<td><input type="radio" value="Male" name="Gender"> Male <input type="radio" value="Female" name="Gender"> Female</td>
+				</tr>
+                
+                <tr>
+					<td>Date of Birth:</td>
+					<td><input type="date" id = "birthday" Name="birthday"></td>
+		</tr>
+        <tr>
+					<td>HSC results:</td>
+					<td><input type="value" age="value"></td>
+		</tr>
+				
+        <tr>
+					<td>SSC results:</td>
+					<td><input type="value" age="value"></td>
+		</tr>
+				
+        <tr>
+					
+		<td>Select a course you want to enroll:</td>
+					<td>
+						<select>
+                    <option value = "CSE"> CSE </opion>
+                    <option value = "EEE"> EEE </opion>
+                    <option value = "BBA"> BBA </opion>
+</select> 
+                    </td>
+					
+					
+					<tr>
+					<td>Choose a year enrolling year:</td>
+					<td>
+						<select>
+                    <option value = "2020"> 2020 </opion>
+                    <option value = "2021"> 2021 </opion>
+                    <option value = "2022"> 2022 </opion>
+</select> 
+                    </td>
+</tr>
+					
+					<tr>
+					<td>Choose a year enrolling year:</td>
+					<td>
+						<select>
+                    <option value = "Summer"> Summer </opion>
+                    <option value = "Fall"> Fall </opion>
+                    <option value = "Spring"> Spring </opion>
+</select> 
+                    </td>
+		</tr>
+        <tr>
+					<td>
+						<input type="button" value="Insert Data"> 
+					</td>
+				</tr>
+			</table>
+			
+			 
+			
+		</form>
+</body>
+<html>
