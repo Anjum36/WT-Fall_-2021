73ef6998b53b46c04b58b281af8dd268518b8b0c
Anjum36 <91002162+Anjum36@users.noreply.github.com>
2021-11-13 19:11:58 -0800

    Add files via upload

---
+++ FTTask1/First.php
@@ -0,0 +1,61 @@
+<!DOCTYPE.html>
+<html>
+	<head>
+    <link rel="stylesheet" href="style.css">
+		<title>Registration</title>
+	</head>
+	<body>
+        <h1> ABC Management System </h1>
+        <h4> We Create Future </h4>
+
+		<h3>Registration Form</h3>
+		<hr/>
+		<form action="" method="post">
+			<table >
+				<tr>
+					<td>First Name:</td>
+					<td><input type="text"  name="name" ></td>
+				</tr>
+				<tr>
+					<td>Last name:</td>
+					<td><input type="text"  name="name" ></td>
+				</tr>
+				<tr>
+					<td>Age:</td>
+					<td><input type="value" age="value"></td>
+		</tr>
+
+				
+				<tr>
+					<td>Designation:</td>
+					<td><input type="radio" value="Junior Programmer" name="Desig"> Junior Programmer <input type="radio" value="Senior Programmer" name="Desig"> Senior Programmer <input type="radio" value="Project Lead" name="Desig"> Project Lead</td>
+				</tr>
+				<tr>
+					<td>Preferred language: </td>
+					<td><input type="checkbox" value="JAVA" name="Preferred language[]">JAVA
+					<input type="checkbox" value="PHP" name="Preferred language[]"> PHP
+					<input type="checkbox" value="C++" name="Preferred language[]"> C++
+					</td>
+				</tr>
+
+				<td>Email:</td>
+					<td><input type="Email" Email="name"></td>
+		</tr>
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" name="pass"></td>
+				</tr>
+				<tr>
+					<td colspan="2" >
+						<input type="submit" name="Submit" value="Submit">
+						<input type="button" value="Reset"> 
+					</td>
+				</tr>
+			</table>
+			
+		
+			
+		</form>
+	</body>
+</html>
+		
